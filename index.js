ed882346209b9ead36dd71a3ecad1b755ed6f23f
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2023-09-20 09:28:41 +0300

    puhelinluettelo backend step3

--- index.js
+++ index.js
@@ -37,6 +37,16 @@ app.get('/info', (req, res) => {
     res.send(`<div><div>Phonebook has info for ${personCount}</div><div>${timestamp.toDateString()} ${timestamp.toTimeString()}</div></div>`)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server is listening port ${PORT}`)
