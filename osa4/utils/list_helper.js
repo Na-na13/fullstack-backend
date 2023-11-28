fc7b2e965c54dd37ea469fb2900155e2c433cd54
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2023-11-28 15:48:34 +0200

    apufunktioita ja yksikkötestejä, step3 step4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,16 +1,47 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
 
 const totalLikes = (blogs) => {
-  const likes = blogs.reduce(function(sum, blog) {
+  const likes = blogs.reduce((sum, blog) => {
     return sum + blog.likes
   }, 0)
 
   return likes
 }
 
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce((likes, blog) => {
+    return (likes.likes > blog.likes ? likes : blog)
+  })
+
+  return {
+    "title": favorite.title, 
+    "author": favorite.author, 
+    "likes": favorite.likes
+  }
+}
+
+const mostBlogs = (blogs) => {
+    const most = _.chain(blogs)
+      .groupBy("author")
+      .mapValues(groupedBlogs => _.size(groupedBlogs))
+      .toPairs()
+      .orderBy([1], "desc")
+      .head()
+      .value()
+
+    return {
+        "author": most[0],
+        "blogs": most[1]
+    }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
