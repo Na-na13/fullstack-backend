acc212b3f820af44e945732f27dfc0e525ff6977
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2023-11-30 12:30:38 +0200

    blogilistan testit, step1

---
+++ osa4/tests/blog_api.test.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('correct number of blogs are returned as json', async () => {
+  const response = await api.get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
