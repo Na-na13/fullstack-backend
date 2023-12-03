ee28864103e01bd82a962ef6ccc9d18cbd9cb1f7
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2023-12-03 20:34:01 +0200

    blogilistan laajennus, step8

--- osa4/utils/middleware.js
+++ osa4/utils/middleware.js
@@ -1,14 +1,28 @@
 const errorHandler = (error, request, response, next) => {
-    console.log('error occurred', error.name)
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'Malformatted id' })
-    } else if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message })
-    } else if (error.name === 'JsonWebTokenError') {
-      return response.status(400).json({ error: 'token missing or invalid' })
-    }
+  console.log('error occurred', error.name)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(400).json({ error: 'token missing or invalid' })
+  }
   
-    next(error)
+  next(error)
 }
 
-module.exports = errorHandler
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('Authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+
+  next()
+}
+
+module.exports = {
+  errorHandler,
+  tokenExtractor
+}
