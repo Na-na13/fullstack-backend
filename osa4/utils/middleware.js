41d8d7dc0ae3d3d982b0df4bd4dc20d47a36a7ed
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2023-12-29 16:53:20 +0200

    token fix

--- osa4/utils/middleware.js
+++ osa4/utils/middleware.js
@@ -14,23 +14,27 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-const tokenExtractor = (request, response, next) => {
+const getTokenFrom = request => {
   const authorization = request.get('Authorization')
   if (authorization && authorization.startsWith('Bearer ')) {
-    request.token = authorization.replace('Bearer ', '')
-  } else {
-    request.token = null
+    return authorization.replace('Bearer ', '')
   }
+    return null
+}
+
+const tokenExtractor = (request, response, next) => {
+  request.token = getTokenFrom(request)
   next()
 }
 
 const userExtractor = async (request, response, next) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  } else {
-    const user = await User.findById(decodedToken.id)
-    request.user = user
+  const token = getTokenFrom(request)
+  if (token) {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    request.user = await User.findById(decodedToken.id)
   }
 
   next()
